2efc9b9d1370e86fd8f4523b990b446693a0c100
Liu Shengpeng <328232234@qq.com>
2016-10-04 16:07:22 +0800

    10c

--- app/models/user.py
+++ app/models/user.py
@@ -5,8 +5,9 @@ from .. import login_manager
 from flask_login import UserMixin, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from datetime import datetime
+import hashlib
 
 
 class User(db.Model, ModelMixin, UserMixin):
@@ -105,6 +106,15 @@ class User(db.Model, ModelMixin, UserMixin):
     def ping(self):
         self.last_seen = datetime.utcnow()
         self.save()
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
