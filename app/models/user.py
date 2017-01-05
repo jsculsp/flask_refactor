87295a276182bfeed6a28c6177f565cb01de3002
jsculsp <328232234@qq.com>
2017-01-05 18:19:03 +0800

    save

--- app/models/user.py
+++ app/models/user.py
@@ -167,10 +167,7 @@ class User(db.Model, ModelMixin, UserMixin):
         self.save()
 
     def gravatar(self, size=100, default='identicon', rating='g'):
-        if request.is_secure:
-            url = 'https://secure.gravatar.com/avatar'
-        else:
-            url = 'http://www.gravatar.com/avatar'
+        url = 'https://www.gravatar.com/avatar'
         hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
